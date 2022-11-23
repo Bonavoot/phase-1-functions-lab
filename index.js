0ed7f2b2e0c9a47236b5d7921e15632daa1d73a4
Joseph <Jbonventre95@gmail.com>
2022-11-23 11:20:00 -0500

    commit tree f8f72c1436811b825a7b74285bf6a3a789d630ab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(dest) {
+  if (42 > dest) {
+    return 42 - dest;
+  } else {
+    return dest - 42;
+  }
+}
+
+const distanceFromHqInFeet = (dest) => {
+  return distanceFromHqInBlocks(dest) * 264;
+};
+
+function distanceTravelledInFeet(start, dest) {
+  if (start > dest) {
+    return (start - dest) * 264;
+  } else if (start < dest) {
+    return (dest - start) * 264;
+  }
+}
+
+function calculatesFarePrice(start, destination) {
+  if (distanceTravelledInFeet(start, destination) < 400) {
+    return 0;
+  } else if (distanceTravelledInFeet(start, destination) < 2000) {
+    return (distanceTravelledInFeet(start, destination) - 400) * 0.02;
+  } else if (
+    distanceTravelledInFeet(start, destination) > 2000 &&
+    distanceTravelledInFeet(start, destination) < 2500
+  ) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
